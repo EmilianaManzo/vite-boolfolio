38b1a371f58cec7ad3e575927c97ea25307c45e4
EmilianaManzo <emiliana.ma96@gmail.com>
2024-05-31 15:54:52 +0200

    [add] milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script>
+  import {store} from './data/store';
   import axios from 'axios';
   import Header from './components/Header.vue';
   import Main from './components/Main.vue';
@@ -12,15 +13,34 @@
       },
       data(){
         return{
-          axios
+          axios,
+          store
         }
+      },
+      methods:{
+        getApi(){
+          axios.get(store.apiUrl)
+            .then(result =>{
+              store.projects = result.data.data;
+              console.log(result.data.data);
+            })
+            .catch(error =>{
+              error = error.message
+            })
+        }
+      },
+      mounted(){
+        this.getApi();
       }
+
   }
 </script>
 
 
 <template>
-  
+  <div>
+    <h1>hello</h1>
+  </div>
 </template>
 
 
